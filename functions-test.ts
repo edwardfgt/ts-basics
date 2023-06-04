4f645f1399a1ee624886f9dcbcf455f75c322e91
Edward Thomas <90358655+edwardfgt@users.noreply.github.com>
2023-06-04 22:51:41 +0100

    Test basic function types

---
+++ functions-test.ts
@@ -0,0 +1,6 @@
+import addNumbers, {addStrings, format} from "./functions";
+
+console.log(addNumbers(1, 2));
+console.log(addStrings("a", "b"));
+console.log(format("a", 10));
+console.log(format("a", "b"));

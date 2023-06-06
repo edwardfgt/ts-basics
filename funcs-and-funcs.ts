15d415935785c8041d4938cdb6d76e43d544fe46
Edward Thomas <90358655+edwardfgt@users.noreply.github.com>
2023-06-06 22:02:06 +0100

    Play around with funtion parameters

---
+++ funcs-and-funcs.ts
@@ -0,0 +1,10 @@
+export function printToFile(text: string, callback: () => void): void {
+    console.log(text);
+    callback();
+}
+
+export function arrayMutate(numbers: number[], mutate: (v: number) => number): number[] {
+    return numbers.map(mutate)
+}
+
+console.log(arrayMutate([1,2,3], (v) => v * 10))
